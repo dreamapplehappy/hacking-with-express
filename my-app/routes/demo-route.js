d1e50116b7c53068fe2c2fba95fa14a9de94729a
dreamapple <2451731631@qq.com>
2017-02-13 23:12:21 +0800

    Guide use middleware

--- my-app/routes/demo-route.js
+++ my-app/routes/demo-route.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+// use middleware
 router.use(function(req, res, next) {
     console.log('DEMO ..');
     next();
@@ -12,6 +13,20 @@ router.get('/', function(req, res) {
 
 router.get('/about', function(req, res) {
     res.send('DEMO / ABOUT');
+});
+
+router.get('/user/:id', function(req, res, next) {
+   if(parseInt(req.params.id) === 0) {
+       next('route');
+   }
+   else {
+       next();
+   }
+}, function(req, res) {
+    res.send('Common User');
+});
+router.get('/user/:id', function(req, res) {
+    res.send('Special Demo User')
 });
 
 module.exports = router;
